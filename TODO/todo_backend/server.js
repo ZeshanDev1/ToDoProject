7fb044cfb2afa84cc5186f171d212b8c6b0848a5
Zeshan Malik <zeshankhaan4@gmail.com>
2025-05-17 20:22:53 +0100

    server.js file newly updated

--- TODO/todo_backend/server.js
+++ TODO/todo_backend/server.js
@@ -7,17 +7,56 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Use environment variable or fallback to localhost
-const mongoURL = process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/TODO';
-
-mongoose.connect(mongoURL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => {
-    console.log('MongoDB connected');
-}).catch((err) => {
-    console.error('MongoDB connection error:', err);
-});
+// Use the correct MongoDB host (defined in docker-compose as "mongo")
+mongoose.connect('mongodb://mongo:27017/TODO', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.error('MongoDB connection error:', err));
 
 app.listen(5000, () => {
-    console.log('Server listening on port: 5000
+  console.log('Server listening on port: 5000');
+});
+
+// Add a new todo
+app.post('/add', (req, res) => {
+  const { task } = req.body;
+  TodoModel.create({ task })
+    .then(result => res.json(result))
+    .catch(err => res.status(500).json(err));
+});
+
+// Get all todos
+app.get('/get', (req, res) => {
+  TodoModel.find()
+    .then(result => res.json(result))
+    .catch(err => res.status(500).json(err));
+});
+
+// Mark a todo as done
+app.put('/edit/:id', (req, res) => {
+  const { id } = req.params;
+  TodoModel.findByIdAndUpdate(id, { done: true }, { new: true })
+    .then(result => res.json(result))
+    .catch(err => res.status(500).json(err));
+});
+
+// Update a todo task
+app.put('/update/:id', (req, res) => {
+  const { id } = req.params;
+  const { task } = req.body;
+  TodoModel.findByIdAndUpdate(id, { task: task }, { new: true })
+    .then(result => res.json(result))
+    .catch(err => res.status(500).json(err));
+});
+
+// Delete a todo
+app.delete('/delete/:id', (req, res) => {
+  const { id } = req.params;
+  TodoModel.findByIdAndDelete(id)
+    .then(result => res.json(result))
+    .catch(err => res.status(500).json(err));
+});
+
+module.exports = app;
